e9ce69ae97d71c30fa6ea479e2475d955180c5ba
Everylisy <anashape710@gmail.com>
2023-04-29 17:41:41 +0900

    refactor: 리액트쿼리 fallback 데이터, 네트워크 에러 시 피드백 toast ui 추가

--- src/hooks/useFetchOrderData.ts
+++ src/hooks/useFetchOrderData.ts
@@ -1,3 +1,4 @@
+import { useToast } from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
 import { fetchOrderData } from "../api/apis";
 import { DROPDOWN_OPTIONS } from "../components/OrderAdmin/FilterDropdown";
@@ -8,40 +9,55 @@ const TODAY_DATE = "2023-03-08";
 
 export const useFetchOrderData = () => {
   const { name, status, sortOption } = useGetSearchParams();
+  const fallback: IOrderData[] | undefined = [];
+  const toast = useToast();
 
-  return useQuery(["orderData"], fetchOrderData, {
-    refetchInterval: 5000,
-    select: (data) => {
-      const orderData: IOrderData[] = data.data
-        ?.filter((v: IOrderData) => v.transaction_time.includes(TODAY_DATE))
+  const { data = fallback, isLoading } = useQuery(
+    ["orderData"],
+    fetchOrderData,
+    {
+      refetchInterval: 5000,
+      onError: (error) => {
+        const title =
+          error instanceof Error
+            ? error.message
+            : "error connecting to the server";
+        toast({ title: title, status: "error" });
+      },
+      select: (data) => {
+        const orderData: IOrderData[] = data.data
+          ?.filter((v: IOrderData) => v.transaction_time.includes(TODAY_DATE))
 
-        .filter((v: IOrderData) => {
-          if (DROPDOWN_OPTIONS.includes(status as string)) {
-            return v.status === (status === "True" ? true : false);
-          }
-          return v;
-        })
+          .filter((v: IOrderData) => {
+            if (DROPDOWN_OPTIONS.includes(status as string)) {
+              return v.status === (status === "True" ? true : false);
+            }
+            return v;
+          })
 
-        .filter((v: IOrderData) => {
-          if (name) {
-            return v.customer_name
-              .toLowerCase()
-              .includes(name?.toLowerCase() as string);
-          }
-          return v;
-        })
+          .filter((v: IOrderData) => {
+            if (name) {
+              return v.customer_name
+                .toLowerCase()
+                .includes(name?.toLowerCase() as string);
+            }
+            return v;
+          })
 
-        .sort((a: IOrderData, b: IOrderData) => {
-          if (sortOption === "id") return b.id - a.id;
-          else if (sortOption === "transaction_time") {
-            return (
-              +new Date(b.transaction_time) - +new Date(a.transaction_time)
-            );
-          }
-          return a.id - b.id;
-        });
+          .sort((a: IOrderData, b: IOrderData) => {
+            if (sortOption === "id") return b.id - a.id;
+            else if (sortOption === "transaction_time") {
+              return (
+                +new Date(b.transaction_time) - +new Date(a.transaction_time)
+              );
+            }
+            return a.id - b.id;
+          });
 
-      return orderData;
+        return orderData;
+      },
     },
-  });
+  );
+
+  return { data, isLoading };
 };
