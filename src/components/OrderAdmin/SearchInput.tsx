2a19deb88bd65c5d0a20383e97fbe11cef32298c
Everylisy <anashape710@gmail.com>
2023-05-03 00:17:53 +0900

    refactor: 고객 이름 검색기능 onChange 할때마다 -> onSubmit으로 수정

--- src/components/OrderAdmin/SearchInput.tsx
+++ src/components/OrderAdmin/SearchInput.tsx
@@ -1,22 +1,27 @@
 import { Input } from "@chakra-ui/react";
+import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 const SearchInput = () => {
+  const [serachName, setSearchName] = useState("");
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const searchInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    searchParams.set("name", e.target.value);
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    searchParams.set("name", serachName);
     setSearchParams(searchParams);
   };
 
   return (
-    <Input
-      placeholder="고객 이름 검색"
-      onChange={searchInputHandler}
-      width="lg"
-      mr={8}
-      value={(searchParams?.get("name") as string) || ""}
-    />
+    <form onSubmit={submitHandler}>
+      <Input
+        placeholder="고객 이름 검색"
+        onChange={(e) => setSearchName(e.target.value)}
+        width="sm"
+        mr={8}
+        value={serachName}
+      />
+    </form>
   );
 };
 
